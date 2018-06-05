85da596488656915eb647b2edc902198db5ae4d6
CarlosOmarRangelHernandez <36654864+CarlosOmarRangelHernandez@users.noreply.github.com>
2018-06-05 14:51:13 +0200

    Update Puerta.ino

--- Puerta.ino
+++ Puerta.ino
@@ -1,41 +1,52 @@
-    #include <avr/sleep.h>
-    
-    int contacto = 9;
-    int led= 8; 
-    
-    void wake ()
-    {
-      // cancel sleep as a precaution
-      sleep_disable();
-      // precautionary while we do other stuff
-      detachInterrupt (0);
-    }  // end of wake
-    
-    void setup () 
-      {
-      digitalWrite (2, HIGH);  // enable pull-up
-          // end of setup
-    
-    
-      pinMode(contacto,INPUT);
-      pinMode(led, OUTPUT); 
-      ADCSRA = 0;  
-      set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
-      sleep_enable();
-      attachInterrupt (contacto, wake, RISING);
-      EIFR = bit (INTF0);  
-        MCUCR = bit (BODS) | bit (BODSE);
-        MCUCR = bit (BODS); 
-        interrupts ();  
-      sleep_cpu ();   
-    }
-    
-    void loop(){
-      if (digitalRead(contacto)==HIGH){
-      
-          digitalWrite(led,HIGH);  
-      }else{
-        digitalWrite(led,LOW); 
-      }
-    }
 
+#include <avr/sleep.h>                    //Se utiliza la libreria sleep
+
+int contacto = 2;                         //variable donde el pin sera el sensor y donde se utilizara la interrrupcion
+int led = 13;                             //variable del pin donde se coloca la alarma visual
+int contador = 0;                         //Variable para contar cuanta veces ah sido abierta
+const int pinBuzzer = 9;
+
+//Metodp para para quitar el modo sleep del arduino
+void despertar () {
+  sleep_disable();                         //desactiva el modos sleep del arduino
+  detachInterrupt (2);                     //Desactiva la interrupcion
+}
+
+
+
+void setup () {
+  Serial.begin(9600);                       //Habilita el puerto serial para enviar informacion a la pc
+  pinMode(contacto, INPUT);                 //variable contacto sera de entrada
+  pinMode(led, OUTPUT);                     //Variable led sera de salida
+  ADCSRA = 0;                               //Minimiza el consumo de energia
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);     //Modo en el que dormira el aduino
+  sleep_enable();                           //Pone a dormir el arduino
+  attachInterrupt (contacto, despertar, RISING); //Activa la interrupcion con la variable contacto, llama al metodo despertar y enciende los pines
+  interrupts ();
+}
+
+void loop() {
+  if (digitalRead(contacto) == HIGH) {       //Se envia un pulso alto al contacto
+    contador++;
+    Serial.print(contador);
+    Serial.print("\n");
+    digitalWrite(led, HIGH);                    //Apaga el led
+
+  } else {
+    digitalWrite(led, LOW);                //Enciende el led
+    buzer();
+  }
+}
+
+void buzer() {
+  tone(pinBuzzer, 440);
+  delay(1000);
+
+  //detener tono durante 500ms
+  noTone(pinBuzzer);
+  delay(500);
+
+  //generar tono de 523Hz durante 500ms, y detenerlo durante 500ms.
+  tone(pinBuzzer, 523, 300);
+  delay(500);
+}
